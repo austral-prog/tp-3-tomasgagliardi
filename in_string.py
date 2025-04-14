55bf8fa265f5b3c75e139aa477764591c6eafaa9
tomasgagliardi <tgagliardi@mail.austral.edu.ar>
2025-04-14 16:39:45 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,14 +1,16 @@
-def check_vowels():
-    # Código a implementar utilizando input.
-    Nombre = input()
-    nombre = Nombre.lower()
+def slice_simple():
+    texto = "Awesome"
+    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
+    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
+    # Para verificar este ejercicio ejecutar el comando
+    # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
 
-    print(f'Contiene a: {"a" in nombre}')
-    print(f'Contiene e: {"e" in nombre}')
-    print(f'Contiene i: {"i" in nombre}')
-    print(f'Contiene o: {"o" in nombre}')
-    print(f'Contiene u: {"u" in nombre}')
-    
+    x = "Awesome"
+    x_real = x.lower()
 
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
+    print(x_real[ :3])    #Imprime las 3 primera letras del texto
+
+    mitad = int(len(x_real)/2)      #me dice cual es la mitad del texto // El int me quita la parte fraccional, no redondea
+    print(x_real[mitad - 1 : mitad + 2])      #DESDE mitad-2 HASTA mitad+1
+
+    print(x_real[ :4] + x[-3: ])
