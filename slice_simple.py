72676c4b8009ad71a71be82f7520f2e37812b1b9
tomasgagliardi <tgagliardi@mail.austral.edu.ar>
2025-04-14 16:39:00 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,7 +1,16 @@
 def slice_simple():
     texto = "Awesome"
-    texto = texto.lower()
-    mitad = int(len(texto)/2)
-    print(texto[0:31])
-    print(texto[ mitad-1: mitad + 2])
-    print(f'{texto[0:4]}{texto[-3:]}')
+    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
+    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
+    # Para verificar este ejercicio ejecutar el comando
+    # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
+
+    x = "Awesome"
+    x_real = x.lower()
+
+    print(x_real[ :3])    #Imprime las 3 primera letras del texto
+
+    mitad = int(len(x_real)/2)      #me dice cual es la mitad del texto // El int me quita la parte fraccional, no redondea
+    print(x_real[mitad - 1 : mitad + 2])      #DESDE mitad-2 HASTA mitad+1
+
+    print(x_real[ :4] + x[-3: ])
